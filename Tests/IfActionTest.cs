eaff5cf8e7ffacfe63e68bb72c694e0431e02e04
yonghun.lim <yonghun.lim@linecorp.com>
2019-10-02 04:10:14 +0900

    Restructuring

---
+++ Tests/IfActionTest.cs
@@ -0,0 +1,9 @@
+﻿using mulova.unicore;
+using UnityEngine;
+
+public class IfActionTest : MonoBehaviour
+{
+    [If(null, IfAction.Error)]public GameObject obj;
+    public int i;
+    [If(0.1f, IfAction.Error)] public float f;
+}
