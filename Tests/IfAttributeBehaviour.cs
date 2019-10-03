6ae49bba1a965a534d178e00c7d229d3275d02ed
yonghun.lim <yonghun.lim@linecorp.com>
2019-10-04 05:48:26 +0900

    PlayTest

---
+++ Tests/IfAttributeBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using mulova.commons;
+using mulova.unicore;
+using UnityEngine;
+
+public class IfAttributeBehaviour : MonoBehaviour
+{
+    [If(null, IfAction.Error)] public GameObject nullErr;
+    [NonNullableField] public GameObject nonNull;
+    public int i;
+    [If(0.1f, IfAction.Error)] public float f;
+
+    public void VerifyAttribute()
+    {
+        FieldAttributeRegistry<VerifyAttribute> reg = new FieldAttributeRegistry<VerifyAttribute>();
+        reg.ForEach(this, (a, f, v) =>
+        {
+            if (!a.IsValid(this, f))
+            {
+                Debug.LogError($"Error {this.name}.{f.Name} = {v}");
+            }
+        });
+    }
+}
---
+++ Tests/VerifyAttributeTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using mulova.commons;
+using mulova.unicore;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class VerifyAttributeTest
+{
+    [UnityTest]
+    public IEnumerator IfAttributeTest()
+    {
+        var inst = new VerifyAttributeClass();
+        yield return null;
+
+        FieldAttributeRegistry<VerifyAttribute> reg = new FieldAttributeRegistry<VerifyAttribute>();
+        reg.ForEach(this, (a, f, v) =>
+        {
+            if (f.Name.StartsWith("null", System.StringComparison.Ordinal))
+            {
+                if (a.IsValid(inst, f))
+                {
+                    Debug.LogError($"[{a.GetType().Name}]{f.Name}.{f.Name} = {v}");
+                }
+            }
+            if (f.Name.StartsWith("nonNull", System.StringComparison.Ordinal))
+            {
+                if (!a.IsValid(inst, f))
+                {
+                    Debug.LogError($"[{a.GetType().Name}]{f.Name}.{f.Name} = {v}");
+                }
+            }
+        });
+    }
+}
+
+public class VerifyAttributeClass
+{
+    [If(null, IfAction.Error)] public GameObject null1;
+    [NonNullableField] public GameObject null2;
+
+    [If(null, IfAction.Error)] public Object nonNull1 = new Object();
+    [NonNullableField] public Object nonNull2 = new Object();
+}
